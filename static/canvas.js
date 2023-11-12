6e738ccb24e6c2f866d97a40d258350f9ffd8a36
ilfey <ilfey.global@gmail.com>
2023-11-13 01:04:59 +0300

    stage

--- static/canvas.js
+++ static/canvas.js
@@ -1,13 +1,15 @@
 const settings = {
   grid: {
-    horizontalCount: 20,
-    verticalCount: 20,
-    size: 40,
+    horizontalCount: 9,
+    verticalCount: 9,
+    size: 30,
     color: "#6b7280",
   },
   item: {
     hidden: {
-      color: "#27272a",
+      borderTopLeft: "#d4d4d4",
+      borderBottomRight: "#737373",
+      color: "#a3a3a3",
     },
     mine: {
       color: "#e11d48",
@@ -16,32 +18,34 @@ const settings = {
       color: "black",
     },
     1: {
-      color: "#27272a",
+      color: "#4f46e5",
     },
     2: {
-      color: "#324545",
+      color: "#16a34a",
     },
     3: {
-      color: "#769782",
+      color: "#e11d48",
     },
     4: {
-      color: "#678952",
+      color: "#581c87",
     },
     5: {
-      color: "#492454",
+      color: "#881337",
     },
     6: {
-      color: "#671346",
+      color: "#059669",
     },
     7: {
-      color: "#576966",
+      color: "#171717",
     },
     8: {
-      color: "#655842",
+      color: "#fafafa",
     },
   },
 };
 
+const timerBlock = document.getElementById("timer");
+const mineCountBlock = document.getElementById("mineCount");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -53,44 +57,137 @@ canvas.width = settings.grid.horizontalCount * settings.grid.size;
 const countHorizontalLines = Math.round(canvas.width / settings.grid.size);
 const countVerticalLines = Math.round(canvas.height / settings.grid.size);
 
-const drawHorizontalLines = (count) => {
-  for (let i = 0; i < count; ++i) {
-    const y = Math.round(canvas.height / count) * i;
-    ctx.beginPath();
-    ctx.strokeStyle = settings.grid.color;
-    ctx.moveTo(0, y);
-    ctx.lineTo(canvas.width, y);
-    ctx.stroke();
-  }
+// const drawHorizontalLines = (count) => {
+//   for (let i = 0; i < count; ++i) {
+//     const y = Math.round(canvas.height / count) * i;
+//     ctx.beginPath();
+//     ctx.strokeStyle = settings.grid.color;
+//     ctx.moveTo(0, y);
+//     ctx.lineTo(canvas.width, y);
+//     ctx.stroke();
+//   }
+// };
+
+// const drawVerticalLines = (count) => {
+//   for (let i = 0; i < count; ++i) {
+//     const x = Math.round(canvas.width / count) * i;
+//     ctx.beginPath();
+//     ctx.strokeStyle = settings.grid.color;
+//     ctx.moveTo(x, 0);
+//     ctx.lineTo(x, canvas.height);
+//     ctx.stroke();
+//   }
+// };
+
+const drawHiddenItem = (x, y) => {
+  ctx.fillStyle = settings.item.hidden.borderBottomRight;
+  ctx.fillRect(x, y, settings.grid.size, settings.grid.size);
+  
+  ctx.beginPath()
+
+  ctx.moveTo(x, y + settings.grid.size);
+  ctx.lineTo(x, y)
+  ctx.lineTo(x + settings.grid.size, y);
+
+  ctx.fillStyle = settings.item.hidden.borderTopLeft;
+  ctx.fill();
+
+  ctx.fillStyle = settings.item.hidden.color;
+  ctx.fillRect(x+4,y+4, settings.grid.size -8, settings.grid.size -8);
 };
 
-const drawVerticalLines = (count) => {
-  for (let i = 0; i < count; ++i) {
-    const x = Math.round(canvas.width / count) * i;
-    ctx.beginPath();
-    ctx.strokeStyle = settings.grid.color;
-    ctx.moveTo(x, 0);
-    ctx.lineTo(x, canvas.height);
-    ctx.stroke();
+const drawRevealedItem = (x, y, color, count) => {
+  ctx.fillStyle = settings.item.hidden.color;
+  ctx.fillRect(
+    column * settings.grid.size,
+    row * settings.grid.size,
+    settings.grid.size,
+    settings.grid.size
+  );
+
+  ctx.fillStyle = settings.item.hidden.color;
+  ctx.fillRect(
+    column * settings.grid.size + 1,
+    row * settings.grid.size + 1,
+    settings.grid.size - 2,
+    settings.grid.size - 2
+  );
+
+  if (count > 0) {
+    ctx.font = "700 18px sans-serif";
+    ctx.fillStyle = color;
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText(
+      count,
+      column * settings.grid.size + settings.grid.size / 2,
+      row * settings.grid.size + settings.grid.size / 2
+    );
   }
-};
+}
 
 const setup = () => {
-  // ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = settings.item.hidden.color;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  console.log(countHorizontalLines, countVerticalLines);
+  for (let i = 0; i < countHorizontalLines; ++i) {
+    for (let j = 0; j < countVerticalLines; ++j) {
+      drawHiddenItem(i * settings.grid.size, j * settings.grid.size);
+    }
+  }
 
-  drawHorizontalLines(countHorizontalLines);
-  drawVerticalLines(countVerticalLines);
+  // console.log(countHorizontalLines, countVerticalLines);
+
+  // drawHorizontalLines(countHorizontalLines);
+  // drawVerticalLines(countVerticalLines);
 };
 
 setup();
 
-const board = makeBoard(countHorizontalLines, countVerticalLines);
-board.onMine = (column, row) => {
-  console.log("Game over");
+const { board, reveal, flag } = createBoard(
+  countHorizontalLines,
+  countVerticalLines
+);
+
+mineCountBlock.innerText = (board.mineCount - board.flagCount)
+  .toString()
+  .padStart(3, "0");
+
+const timer = createTimer(timerBlock);
+
+const onLeftClick = (column, row) => {
+  // console.log(`Reveal on ${column}, ${row}`);
+  reveal(column, row);
+};
+
+const onRightClick = (column, row) => {
+  // console.log(`Flag ${column}, ${row}`);
+  flag(column, row);
+};
+
+canvas.addEventListener("contextmenu", (e) => e.preventDefault());
+canvas.addEventListener("mousedown", (e) => {
+  const rect = canvas.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+
+  const column = Math.floor(x / settings.grid.size);
+  const row = Math.floor(y / settings.grid.size);
+
+  if (e.button === 0) {
+    onLeftClick(column, row);
+  } else if (e.button === 2) {
+    onRightClick(column, row);
+  }
+});
+
+const onReveal = (column, row, item) => {
+  console.log(`[onReveal] Reveal ${column} x ${row} cell`);
+};
+
+const onMine = (column, row) => {
+  console.log("[onMine] Game over");
 
   ctx.beginPath();
   ctx.fillStyle = settings.item.hidden.color;
@@ -110,34 +207,9 @@ board.onMine = (column, row) => {
   );
 };
 
-canvas.addEventListener("click", (e) => {
-  const rect = canvas.getBoundingClientRect();
-  const x = e.clientX - rect.left;
-  const y = e.clientY - rect.top;
+const onSpace = (column, row, item) => {
+  console.log(`[onSpace] Space on ${column}, ${row}`);
 
-  const column = Math.floor(x / settings.grid.size);
-  const row = Math.floor(y / settings.grid.size);
-
-  board.reveal(column, row);
-
-  // console.log(`Left clicked on ${column}, ${row}`);
-});
-
-canvas.addEventListener("contextmenu", (e) => {
-  e.preventDefault();
-  const rect = canvas.getBoundingClientRect();
-  const x = e.clientX - rect.left;
-  const y = e.clientY - rect.top;
-
-  const column = Math.floor(x / settings.grid.size);
-  const row = Math.floor(y / settings.grid.size);
-
-  board.flag(column, row);
-
-  console.log(`Right clicked on ${column}, ${row}`);
-});
-
-board.onReveal = (column, row, item) => {
   ctx.fillStyle = settings.item.hidden.color;
   ctx.fillRect(
     column * settings.grid.size,
@@ -146,7 +218,7 @@ board.onReveal = (column, row, item) => {
     settings.grid.size
   );
 
-  ctx.fillStyle = settings.item[item.countAdjacentMines].color;
+  ctx.fillStyle = settings.item.hidden.color;
   ctx.fillRect(
     column * settings.grid.size + 1,
     row * settings.grid.size + 1,
@@ -155,8 +227,8 @@ board.onReveal = (column, row, item) => {
   );
 
   if (item.countAdjacentMines > 0) {
-    ctx.font = "18px sans-serif";
-    ctx.fillStyle = "white";
+    ctx.font = "700 18px sans-serif";
+    ctx.fillStyle = settings.item[item.countAdjacentMines].color;
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
     ctx.fillText(
@@ -166,3 +238,53 @@ board.onReveal = (column, row, item) => {
     );
   }
 };
+
+const onStart = () => {
+  console.log("[onStart]");
+
+  timer.start();
+};
+
+const onFail = () => {
+  console.log("[onFail]");
+
+  timer.stop();
+};
+
+const onVictory = () => {
+  console.log("[onVictory]");
+
+  timer.stop();
+};
+
+const onFlug = () => {
+  console.log("[onFlug]");
+
+  mineCountBlock.innerText = (board.mineCount - board.flagCount)
+    .toString()
+    .padStart(3, "0");
+};
+
+const onUnflug = () => {
+  console.log("[onUnflag]");
+
+  mineCountBlock.innerText = (board.mineCount - board.flagCount)
+    .toString()
+    .padStart(3, "0");
+};
+
+const events = [
+  { onReveal },
+  { onMine },
+  { onSpace },
+  { onStart },
+  { onFail },
+  { onVictory },
+  { onFlug },
+  { onUnflug },
+];
+
+events.forEach((obj) => {
+  const [event, callback] = Object.entries(obj)[0];
+  appendEventListener(board, event, callback);
+});
--- static/game.js
+++ static/game.js
@@ -1,5 +1,10 @@
 let count = 0;
 
+/**
+ * Creates a new item object.
+ *
+ * @return {object} The newly created item.
+ */
 const createItem = () => {
   const item = {
     isMine: Math.round(Math.random()) === 1,
@@ -9,149 +14,256 @@ const createItem = () => {
     countAdjacentMines: 0,
   };
 
-  // if (count < 10) {
-  //   item.isMine = true;
-  //   ++count;
-  // }
-
   return item;
 };
 
-const makeBoard = (columns, rows /* , onCreate */) => {
-  console.log(`Creating board with ${columns} x ${rows} cells`);
+/**
+ * Calculates the number of adjacent mines for each item on the board.
+ *
+ * @param {object} board - The board object containing information about the board.
+ */
+const calcAdjacentMines = (board) => {
+  const { columns, rows, items } = board;
 
-  const mineCount = Math.round((columns * rows) / 4); // 20% of cells are mines
-
-  const board = {
-    _items: Array.from({ length: columns * rows }, () => createItem()),
-    _isRevialing: false,
-
-    revealAll: () => {
-      board._isRevialing = true;
-      board._items.forEach((item, index) => {
-        const row = Math.floor(index / rows);
-        const column = index % rows;
-
-        board.reveal(column, row);
-      });
-      board._isRevialing = false;
-    },
-    _revealZero: (column, row) => {
-      // Check top cell
-      if (row > 0) {
-        board.reveal(column, row - 1);
-      }
-
-      // Check left cell
-      if (column > 0) {
-        board.reveal(column - 1, row);
-      }
-
-      // Check bottom cell
-      if (row < rows - 1) {
-        board.reveal(column, row + 1);
-      }
-
-      // Check right cell
-      if (column < columns - 1) {
-        board.reveal(column + 1, row);
-      }
-    },
-    reveal: (column, row) => {
-      const index = rows * row + column;
-
-      // console.log(`Revealing cell ${index} [${column}, ${row}]`);
-
-      const item = board._items[index];
-
-      if (item.isFlagged || item.isRevealed) {
-        return;
-      }
-
-      item.isRevealed = true;
-
-      if (item.isMine) {
-        if (!board._isRevialing) {
-          board.revealAll();
-        }
-
-        board.onMine(column, row);
-        return;
-      }
-
-      if (!board._isRevialing && item.isRevealed && item.countAdjacentMines === 0) {
-        board._revealZero(column, row);
-      }
-
-      board.onReveal(column, row, item);
-    },
-    flag: (column, row) => {
-      const index = rows * row + column;
-
-      const item = board._items[index];
-
-      if (item.isFlagged) {
-        console.log(`Unflagging cell ${index} [${column}, ${row}]`);
-
-        item.isFlagged = false;
-
-        board.onUnflug(column, row);
-      } else {
-        console.log(`Flagging cell ${index} [${column}, ${row}]`);
-
-        item.isFlagged = true;
-        board.onFlug(column, row)
-      }
-    },
-
-    onMine: (column, row) => {},
-    onReveal: (column, row, item) => {},
-    onFlug: (column, row) => {},
-    onUnflug: (column, row) => {},
-  };
-
-  board._items.forEach((item, index) => {
+  items.forEach((item, index) => {
     if (item.isMine) {
       return;
     }
 
-    const row = Math.floor(index / rows);
-    const column = index % rows;
-
-    // console.log(`Checking cell ${index} [${column}, ${row}]`);
+    const row = Math.floor(index / board.rows);
+    const column = index % board.rows;
 
     if (row > 0) {
-      item.countAdjacentMines += board._items[index - rows].isMine ? 1 : 0; // top
+      item.countAdjacentMines += items[index - rows].isMine ? 1 : 0; // top
 
       if (column > 0) {
-        item.countAdjacentMines += board._items[index - rows - 1].isMine ? 1 : 0; // top-left
+        item.countAdjacentMines += items[index - rows - 1].isMine ? 1 : 0; // top-left
       }
 
       if (column < columns - 1) {
-        item.countAdjacentMines += board._items[index - rows + 1].isMine ? 1 : 0; // top-right
+        item.countAdjacentMines += items[index - rows + 1].isMine ? 1 : 0; // top-right
       }
     }
 
     if (column < columns - 1) {
-      item.countAdjacentMines += board._items[index + 1].isMine ? 1 : 0; // right
+      item.countAdjacentMines += items[index + 1].isMine ? 1 : 0; // right
     }
 
     if (row < rows - 1) {
-      item.countAdjacentMines += board._items[index + rows].isMine ? 1 : 0; // bottom
+      item.countAdjacentMines += items[index + rows].isMine ? 1 : 0; // bottom
 
       if (column > 0) {
-        item.countAdjacentMines += board._items[index + rows - 1].isMine ? 1 : 0; // bottom-left
+        item.countAdjacentMines += items[index + rows - 1].isMine ? 1 : 0; // bottom-left
       }
 
       if (column < columns - 1) {
-        item.countAdjacentMines += board._items[index + rows + 1].isMine ? 1 : 0; // bottom-right
+        item.countAdjacentMines += items[index + rows + 1].isMine ? 1 : 0; // bottom-right
       }
     }
 
     if (column > 0) {
-      item.countAdjacentMines += board._items[index - 1].isMine ? 1 : 0; // left
+      item.countAdjacentMines += items[index - 1].isMine ? 1 : 0; // left
     }
   });
+};
 
-  return board;
+/**
+ * Reveals all items on the board.
+ *
+ * @param {object} board - The board object containing the items.
+ * @return {void} This function does not return a value.
+ */
+const revealAll = (board) => {
+  board._isRevialing = true;
+
+  board.items.forEach((item, index) => {
+    const row = Math.floor(index / board.rows);
+    const column = index % board.rows;
+
+    reveal(column, row);
+  });
+
+  board._isRevialing = false;
+};
+
+/**
+ * Reveals the neighboring cells of a given cell on the board.
+ *
+ * @param {number} board - the game board
+ * @param {number} column - the column of the cell
+ * @param {number} row - the row of the cell
+ */
+const revealCloud = (board, column, row) => {
+  // Check top cell
+  if (row > 0) {
+    reveal(column, row - 1);
+  }
+
+  // Check left cell
+  if (column > 0) {
+    reveal(column - 1, row);
+  }
+
+  // Check bottom cell
+  if (row < board.rows - 1) {
+    reveal(column, row + 1);
+  }
+
+  // Check right cell
+  if (column < board.columns - 1) {
+    reveal(column + 1, row);
+  }
+};
+
+/**
+ * Adds an event listener to the board for a specific event type.
+ *
+ * @param {object} board - The board object.
+ * @param {string} eventType - The type of event to listen for.
+ * @param {function} callback - The callback function to be called when the event is triggered.
+ */
+const appendEventListener = (board, eventType, callback) => {
+  console.log(`[addEventListener] ${eventType}`);
+
+  board.events[eventType].push(callback);
+};
+
+/**
+ * Triggers an event on the board.
+ *
+ * @param {object} board - The board object.
+ * @param {string} eventType - The type of event to trigger.
+ * @param {number} column - The column index.
+ * @param {number} row - The row index.
+ * @param {object} item - The item associated with the event.
+ */
+const triggerEvent = (board, eventType, column, row, item) => {
+  board.events[eventType].forEach((callback) => {
+    callback(column, row, item);
+  });
+};
+
+/**
+ * Remove an event listener from the board.
+ *
+ * @param {object} board - The board object.
+ * @param {string} eventType - The type of event.
+ * @param {function} callback - The callback function to remove.
+ */
+const removeEventListener = (board, eventType, callback) => {
+  const events = board.events[eventType];
+  const eventIndex = events.indexOf(callback);
+  events = events.splice(eventIndex, 1);
+};
+
+/**
+ * Creates a game board with the specified number of columns and rows.
+ *
+ * @param {number} columns - The number of columns in the game board.
+ * @param {number} rows - The number of rows in the game board.
+ * @return {object} An object containing the game board and two functions,
+ * reveal and flag, for interacting with the game board.
+ */
+const createBoard = (columns, rows) => {
+  console.log(`[createBoard] Creating board with ${columns} x ${rows} cells`);
+
+  const board = {
+    columns,
+    rows,
+    flagCount: 0,
+    mineCount: Math.round((columns * rows) / 5), // 20% of cells are mines
+    items: Array.from({ length: columns * rows }, () => createItem()),
+    _isRevialing: false,
+    _isFirstReveal: true,
+    events: {
+      onStart: [],
+      onFail: [],
+      onVictory: [],
+      onMine: [],
+      onSpace: [],
+      onReveal: [],
+      onFlug: [],
+      onUnflug: [],
+    },
+  };
+
+  /**
+   * Reveals a cell on the game board.
+   *
+   * @param {number} column - The column index of the cell to reveal.
+   * @param {number} row - The row index of the cell to reveal.
+   */
+  const reveal = (column, row) => {
+    const index = board.rows * row + column;
+    const item = board.items[index];
+
+    if (item.isFlagged || item.isRevealed) {
+      return;
+    }
+
+    if (board._isFirstReveal) {
+      board._isFirstReveal = false;
+      triggerEvent(board, "onStart", column, row, item);
+    }
+
+    item.isRevealed = true;
+
+    if (item.isMine) {
+      if (!board._isRevialing) {
+        triggerEvent(board, "onFail", column, row, item);
+        revealAll(board);
+      }
+
+      triggerEvent(board, "onMine", column, row, item);
+      return;
+    }
+
+    if (
+      !board._isRevialing &&
+      item.isRevealed &&
+      item.countAdjacentMines === 0
+    ) {
+      revealCloud(board, column, row);
+    }
+
+    triggerEvent(board, "onSpace", column, row, item);
+  };
+
+  /**
+   * Flags or unflags a cell on the board.
+   *
+   * @param {number} column - the column index of the cell
+   * @param {number} row - the row index of the cell
+   */
+  const flag = (column, row) => {
+    const index = board.rows * row + column;
+    const item = board.items[index];
+
+    if (board.mineCount <= board.flagCount) {
+      console.log("[flag] Not enough mines left to flag");
+      return;
+    }
+
+    if (item.isFlagged) {
+      console.log(`[flag] Unflagging cell ${index} [${column}, ${row}]`);
+      --board.flagCount
+      item.isFlagged = false;
+      triggerEvent(board, "onUnflug", column, row, item);
+      return;
+    }
+
+    console.log(`[flag] Flagging cell ${index} [${column}, ${row}]`);
+
+    ++board.flagCount;
+    item.isFlagged = true;
+    triggerEvent(board, "onFlug", column, row, item);
+  };
+
+  calcAdjacentMines(board);
+
+  return {
+    board,
+    reveal,
+    flag,
+  };
 };
